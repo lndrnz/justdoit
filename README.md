d64f619bab06863fab438363d03ac0db8df88372
Lander Nunez <landernunez@gmail.com>
2023-11-03 09:41:13 -0700

    eslint and updated ReadMe

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
-### JustDoIt
-A small project using the MERN stack that allows a user to create tasks, mark them complete, and then delete them.
+# JustDoIt
 
-##Purpose
-This was to better my skills with MERN stack in order to big more complex projects later on.
+A small project using the MERN stack that allows a user to create tasks, mark them as complete, and then delete them.
+
+## Purpose
+
+The purpose of this project is to familiarize myself with MERN (MongoDB, Express, React, Node)
+
+## Features
+
+- Create, update, and delete tasks.
+- Mark tasks as complete.
+- Intuitive user interface for managing tasks.
+
+## Installation and Setup
+
+To run this project on your local machine, follow these steps:
+
+1. Clone the repository
+2. cd into both client and api and npm i
+3. Make sure MongoDB is installed and turned on
+4. cd into api and run npm start
+5. cd into client and run npm start
+6. Go to http://localhost:3000/
+
+![Image](Images/Image.png)
--- api/server.js
+++ api/server.js
@@ -1,49 +1,50 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb://127.0.0.1:27017/mern-todo", {
+mongoose
+  .connect("mongodb://127.0.0.1:27017/mern-todo", {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(() => console.log("Connected to DB"))
-    .catch(console.error);
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to DB"))
+  .catch(console.error);
 
-const Todo = require('./models/Todo');
+const Todo = require("./models/Todo");
 
-app.get('/todos', async (req, res) => {
-    const todos = await Todo.find();
-    
-    res.json(todos);
-})
+app.get("/todos", async (req, res) => {
+  const todos = await Todo.find();
 
-app.post('/todo/new', (req, res) => {
-    const todo = new Todo({
-        text: req.body.text
-    });
+  res.json(todos);
+});
 
-    todo.save();
+app.post("/todo/new", (req, res) => {
+  const todo = new Todo({
+    text: req.body.text,
+  });
 
-    res.json(todo);
-})
+  todo.save();
 
-app.delete('/todo/delete/:id', async(req, res) => {
-    const result = await Todo.findByIdAndDelete(req.params.id);
-    res.json(result);
-})
+  res.json(todo);
+});
 
-app.get('/todo/complete/:id', async(req, res) => {
-    const todo = await Todo.findById(req.params.id);
+app.delete("/todo/delete/:id", async (req, res) => {
+  const result = await Todo.findByIdAndDelete(req.params.id);
+  res.json(result);
+});
 
-    todo.complete = !todo.complete;
+app.get("/todo/complete/:id", async (req, res) => {
+  const todo = await Todo.findById(req.params.id);
 
-    todo.save();
-    res.json(todo);
-})
+  todo.complete = !todo.complete;
+
+  todo.save();
+  res.json(todo);
+});
 
 app.listen(3001, () => console.log("Server started on Port 3001"));
